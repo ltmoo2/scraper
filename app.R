32cdef65100f6b15bafe97b289903d3aece50bfb
Lachlan Moody <ltmoo2@student.monash.edu>
2021-03-10 00:52:22 +1100

    Deployed working app to GitHub

---
+++ app.R
@@ -0,0 +1,154 @@
+library(shiny)
+library(rvest)
+library(dplyr)
+library(zoo)
+library(stringr)
+library(tidyr)
+library(purrr)
+library(lubridate)
+library(readr)
+
+ui <- fluidPage(
+    
+    
+    titlePanel("Real Commercial Web Scraper"),
+    
+    
+    sidebarLayout(
+        sidebarPanel(
+            textInput("url",
+                      "Paste URL"),
+            dateInput("date",
+                      "Select Earliest Lease",
+                      value = Sys.Date()),
+            numericInput("pgs", 
+                         "Select Number of Pages",
+                         value = 5),
+            actionButton("create",
+                         "Scrape"),
+            downloadButton("download", "Download Data")
+        ),
+        
+        
+        mainPanel(
+            dataTableOutput("test")
+        )
+    )
+)
+
+
+server <- function(input, output) {
+    
+    
+    pages <- reactive({
+        
+        req(input$create)
+        
+        pages <- paste0(input$url, "&page=", c(1:input$pgs))
+    })
+    
+    final_table <- reactive({
+        
+        req(input$create)
+        
+        final_table <- data.frame()
+        
+        links_final <- data.frame()
+        
+        
+        for(i in unique(pages())){
+            
+            webpage <- read_html(i)
+            
+            links <- html_nodes(webpage, 'a')%>%
+                html_attr("href")
+            
+            links_df <- as_tibble(links) 
+            
+            links_clean <- links_df %>%
+                filter(str_detect(value, "leased/property")) %>%
+                distinct() %>%
+                mutate(value = paste0("https://www.realcommercial.com.au", value))%>%
+                mutate("id" = row_number())
+            
+            for(i in unique(links_clean$id)){
+                
+                page <- links_clean %>%
+                    filter(id == i)
+                
+                result <- read_html(page$value)
+                
+                address <- html_nodes(result, "h1") %>%
+                    html_text() %>%
+                    as_tibble()%>%
+                    rename("Address" = value)
+                
+                attr_desc <- html_nodes(result, ".Attribute_label_1bYjg") %>%
+                    html_text() %>%
+                    as_tibble() %>%
+                    rename("attr_desc" = value)
+                
+                attr_val <- html_nodes(result, ".Attribute_value_i8Dee") %>%
+                    html_text() %>%
+                    as_tibble()
+                
+                attr <- attr_desc %>%
+                    bind_cols(attr_val) %>%
+                    pivot_wider(names_from = attr_desc, values_from = value)
+                
+                desc <- html_nodes(result, ".DescriptionPanel_description_20faq") %>%
+                    html_text() %>%
+                    as_tibble() %>%
+                    rename("Description" = value)
+                
+                agency <- html_nodes(result, ".AgencyPanel_agencyNameLink_nCd-h")%>%
+                    html_text() %>%
+                    as_tibble() %>%
+                    top_n(1) %>%
+                    rename("Agency" = value)
+                
+                agent <- html_nodes(result, ".AgentDetails_name_23QWU") %>%
+                    html_text() %>%
+                    as_tibble() %>%
+                    top_n(1) %>%
+                    rename("Agent" = value)
+                
+                property <- bind_cols(address, attr, desc, agency, agent) %>%
+                    select(Address, `Floor area`, `Leased on`, Description, Agency, Agent) %>%
+                    mutate("Source" = paste(page$value))
+                
+                final_table <- rbind(final_table, property)
+                
+            }
+            
+        }
+        
+        final_table <- final_table %>%
+            mutate("Leased on" = dmy(`Leased on`)) %>%
+            filter(`Leased on` >= input$date)
+        
+        
+        return(final_table)
+    })
+    
+    
+    
+    output$test <- renderDataTable({final_table()},
+                                   options = list(pageLength = 25, info = FALSE,
+                                                  lengthMenu = list(c(25, -1), c("25", "All"))))
+    
+    output$download <- downloadHandler(
+        filename = function() {
+            paste0(Sys.Date(), " - Scrape.csv")
+        },
+        content = function(file) {
+            write_csv(final_table(), file)
+        }
+    )
+    
+    
+    
+}
+
+
+shinyApp(ui = ui, server = server)
